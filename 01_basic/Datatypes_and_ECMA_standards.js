50678c986a8b37395e3ef352d026b2018b7d75ce
ashishkumarrsm <135937984+ashishkumarrsm@users.noreply.github.com>
2024-08-09 11:27:47 +0000

    DataType class

---
+++ 01_basic/Datatypes_and_ECMA_standards.js
@@ -0,0 +1,37 @@
+"use strict";//treat all js code as newr version
+
+console.log("Datatypes and ECMA standards")
+// alert(3+3)
+
+// we are ysin node js so thats why alert not work 
+
+
+console.log(3 + 3); 
+console.log("Ashish");
+
+
+let name ="Ashish" 
+let age =21
+let isLoggedIn= false
+let state;
+
+
+// primtive datatype
+// number => 2 to power 53
+// bigint => 
+// String => "" ''
+// boolean => true/false
+// null => stanalone value
+// undifined => 
+// symbol => unique 
+
+
+
+
+// non primtive datatype
+// object 
+console.log(typeof "Vikas");
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);//null is objuct datatype
+
---
+++ 01_basic/Let_const_and_var_ki_kahani .js
@@ -0,0 +1,22 @@
+console.log("Let, const and var ki kahani ");
+
+const accountId = 11122
+let accountEmail = "Vikas141199@12.gmail.com" 
+var accountPassword = "12345"
+accountCity = "Kurukshetra"
+let accountState;
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+
+
+
+// accountId = 1
+// console.log(accountId);
+
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
