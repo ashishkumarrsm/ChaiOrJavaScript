c3018fe002367e81de9f807543ac12ef901c20b1
ashishkumarrsm <135937984+ashishkumarrsm@users.noreply.github.com>
2024-08-10 03:33:37 +0000

    Datatype conversion confusion

---
+++ 01_basic/Datatype_conversion_confusion.js
@@ -0,0 +1,66 @@
+console.log("Hallo");
+
+
+// let score ="33abc"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)  //NaN 
+
+// let score =null
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)  
+
+
+// let score =undefined
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)  //NaN 
+
+
+
+// "33"=> 33
+// "33abv"= >NaN
+// true => ;false => 0
+
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn);
+
+// let isLoggedIn =""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn);
+
+
+// let isLoggedIn ="Ashish "
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log( booleanIsLoggedIn);
+
+
+
+// 1 => true;0 => false 
+// "" => false
+// " " => true
+//  "Ashish " => true
+
+
+
+let sumNumber =33
+let stringNumber = String(sumNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
