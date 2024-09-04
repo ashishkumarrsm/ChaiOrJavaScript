c45414a48abf21a0010cddac031642e397708015
ashishkumarrsm <135937984+ashishkumarrsm@users.noreply.github.com>
2024-09-04 12:34:27 +0000

    Data type call by value and refrance type

--- 01_basic/Data_types_of_javascript_summary.js
+++ 01_basic/Data_types_of_javascript_summary.js
@@ -1,13 +1,51 @@
 console.log("hello");
+//  JavaScript is a dynamically typed language, but TypeScript is a statically typed language.
+
 // primtive
+//  call by value 
 // 7 type : ssbbnnu
 // 1: string
 // 2: number
 // 3: symbole
+const id= Symbol('123')
+const another=Symbol('123')
+console.log(id === another );
+
 // 4: bollion
 // 5: bigInt
+
 // 6: null
 // 7: undefined
 
 
-// non prmitive or refrance type 
+// non prmitive 
+// or 
+// refrance type 
+
+
+// 1 Array
+const heros =[ "spiderman","superman"]
+// 2 objects
+ const obj = {
+    name:"vikas",
+    class:"Mca",
+    age : 23
+
+ }
+// 3 functions 
+
+const myFunction= function(){
+   console.log("hello");
+   
+}
+myFunction()
+
+
+// An arrow function expression is a compact alternative to a traditional function expression, with some semantic differences and deliberate limitations in usage:
+
+let mySecondFunction = ()=>{
+ console.log("welcome helo ");
+ 
+}
+
+mySecondFunction()
