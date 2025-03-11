2710926ac7093a6319a84f82df9401193055227c
ashishkumarrsm <135937984+ashishkumarrsm@users.noreply.github.com>
2025-03-11 06:38:51 +0000

    promise

---
+++ 01_basic/class&obj.js
@@ -0,0 +1,30 @@
+class car{
+    constructor(make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    startEngine(){
+        console.log("Engine started");
+    }
+    accelerate(){
+        console.log("Accelerating");
+    }
+    brake(){
+        console.log("Braking");
+    }
+    getDetails(){
+        console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
+    }
+    static getAverageAge(carList){
+        let totalAge = 0;
+        for(let car of carList){
+            totalAge += car.year;
+        }
+        return totalAge / carList.length;
+    }
+}
+
+
+let myobj=new  car();
+console.log(myobj.startEngine(), myobj.accelerate, myobj);
